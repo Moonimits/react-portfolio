bb9151008cd535f0b0ed806fbe7d3154f69bd400
Marco Luis Hernandez <marcoluis.hernandez@gmail.com>
2025-06-22 18:27:23 +0800

    jitter fix

--- src/components/AnimateIn.jsx
+++ src/components/AnimateIn.jsx
@@ -17,7 +17,7 @@ export function SlideLeft({ children }) {
     <motion.div
       initial={{ opacity: 0, x: 100 }}
       whileInView={{ opacity: 1, x: 0 }}
-      transition={{ duration: .5 }}
+      transition={{ duration: 0.5 }}
       viewport={{once: true}}
     >
       {children}
